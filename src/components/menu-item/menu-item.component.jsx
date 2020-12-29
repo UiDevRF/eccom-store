83e49f25b8eb4766590e5bf188fedf5767aeef01
RFK <syeda.uidev@gmail.com>
2020-12-29 16:51:39 -0600

    Add some styling and fonts as well as break our app component in smaller components

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import '../menu-item/menu-item.styles.scss'
 
-const MenuItem = ({ title , imageUrl , size}) => (
-
-    
-    <div style={{
+const MenuItem = ({ title , imageUrl , size}) => (   
+    <div className={`${size} menu-item`} >
+    <div className='background-image' style={{
         backgroundImage: `url(${imageUrl})`
-    }} 
-    className={`${size} menu-item`} 
-    >
+    }} /> 
             <div className='content'>
                 <h1 className='title'>{title}</h1>
                 <span className='subtitle'>SHOP NOW</span>
             </div> 
-              
+            
+
     </div>
 
 )
