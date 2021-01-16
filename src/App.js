1fabb0c5d04d49c67cd9d338edd74a02c19f13c6
RFK <syeda.uidev@gmail.com>
2021-01-16 12:09:02 -0600

    added handle submit for signIn component

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends React.Component {
            }
          },
          () => {
-           console.group(this.state);
+           console.log(this.state);
          });
          //console.log(snapshot.data());
        });
