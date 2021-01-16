755e7572ac53f4547d6f3ce9ad0403778ec37853
RFK <syeda.uidev@gmail.com>
2021-01-15 23:22:18 -0600

    partially added sign up form

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component';
 import SignInSignUpPage from './pages/signIn-signUp/signIn-signUp-page.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor(){
@@ -18,10 +18,30 @@ class App extends React.Component {
   unsubscribeFromAuth = null ;
 
   componentDidMount(){
-   this.unsubscribeFromAuth =  auth.onAuthStateChanged(user => {
-      this.setState({currentUser:user})
+   this.unsubscribeFromAuth =  auth.onAuthStateChanged(async (userAuth) => {
+     if(userAuth){
+       const userRef = await createUserProfileDocument(userAuth);
+       userRef.onSnapshot(snapshot => {
+         this.setState({
+           currentUser: {
+             id: snapshot.id,
+             ...snapshot.data()
+           }
+         },
+         () => {
+           console.group(this.state);
+         });
+         //console.log(snapshot.data());
+       });
 
-      console.log(user);
+     }
+
+     else{
+       this.setState({currentUser:userAuth});
+     }
+    
+
+
     })
 
   }
--- src/pages/signIn-signUp/signIn-signUp-page.component.jsx
+++ src/pages/signIn-signUp/signIn-signUp-page.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './signIn-signUp-page.scss';
 
 const SignInSignUpPage = () => (
  <div className='signIn-signUp-page'> 
- <SignIn /> 
+ <SignIn />
+ <SignUp /> 
  </div>
 
 
