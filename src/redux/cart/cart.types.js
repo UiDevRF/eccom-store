231b43086246ab35926a0297cf6f24799db182f9
RFK <syeda.uidev@gmail.com>
2021-01-22 11:34:36 -0600

    Updated Shop data and Added Cart component

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,6 @@
 const CartActionTypes =  {
-    TOGGLE_CART_HIDDEN : 'TOGGLE_CART_HIDDEN'
+    TOGGLE_CART_HIDDEN : 'TOGGLE_CART_HIDDEN',
+    addItem: 'ADD_ITEM'
 } 
 
 export default CartActionTypes;
